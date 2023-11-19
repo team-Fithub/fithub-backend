553b69c35d57fabb2cd92a15d8748c88cad696f7
mohyerolo <garam2101@naver.com>
2023-11-19 14:08:20 +0900

    feat: securityConfig 추가 (jwt 작업 완료 후 설정 추가)

---
+++ src/main/java/com/fithub/fithubbackend/global/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.fithub.fithubbackend.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    // TODO: 토큰 프로바이더 추가
+
+    // TODO: 로그인, 회원가입 패턴으로 수정
+    private static final String[] PERMIT_ALL_PATTERNS = new String[] {
+            "/", "/auth/**"
+    };
+
+    private static final String[] PERMIT_ALL_GET_PATTERNS = new String[] {
+        "/users/training/**"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                // TODO: jwtFilter 추가
+                // .addFilterBefore()
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers(
+                                Arrays.stream(PERMIT_ALL_PATTERNS).map(AntPathRequestMatcher::antMatcher).toArray(AntPathRequestMatcher[]::new)
+                        ).permitAll()
+                        .requestMatchers(
+                                HttpMethod.GET, PERMIT_ALL_GET_PATTERNS).permitAll()
+                        .anyRequest().authenticated()
+                )
+                // TODO: exceptionHandling, oauth2 설정 추가
+                .build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                .requestMatchers("/swagger-ui/**");
+    }
+}
