6f2c040faece6236b9b6cda20c26fcffe4845201
mohyerolo <garam2101@naver.com>
2023-11-19 14:38:39 +0900

    update: training 컨트롤러 swagger 설명 추가

--- src/main/java/com/fithub/fithubbackend/domain/Training/api/UserTrainingController.java
+++ src/main/java/com/fithub/fithubbackend/domain/Training/api/UserTrainingController.java
@@ -3,6 +3,8 @@ package com.fithub.fithubbackend.domain.Training.api;
 import com.fithub.fithubbackend.domain.Training.application.UserTrainingService;
 import com.fithub.fithubbackend.domain.Training.dto.TrainingInfoDto;
 import com.fithub.fithubbackend.domain.Training.dto.TrainingOutlineDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,12 +22,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserTrainingController {
     private final UserTrainingService userTrainingService;
 
+    @Operation(summary = "트레이닝 전체 조회, page 사용 (size = 9, sort=\"id\" desc 적용되어있음. swagger에서 보낼 때 따로 지정하는 거 없으면 parameter에 pageable은 다 지우고 보내도 됨)")
     @GetMapping("/all")
     // TODO: Security permitAll, filter X 추가
-    public ResponseEntity<Page<TrainingOutlineDto>> searchAll(@PageableDefault(size = 10, sort = "id",  direction = Sort.Direction.DESC) Pageable pageable) {
+    public ResponseEntity<Page<TrainingOutlineDto>> searchAll(@PageableDefault(size = 9, sort = "id",  direction = Sort.Direction.DESC) Pageable pageable) {
         return ResponseEntity.ok(userTrainingService.searchAll(pageable));
     }
 
+    @Operation(summary = "트레이닝 하나 상세 조회", parameters = {
+            @Parameter(name = "trainingId", description = "조회할 트레이닝의 primary key(id)")
+    })
     @GetMapping
     public ResponseEntity<TrainingInfoDto> searchById(@RequestParam Long trainingId) {
         return ResponseEntity.ok(userTrainingService.searchById(trainingId));
