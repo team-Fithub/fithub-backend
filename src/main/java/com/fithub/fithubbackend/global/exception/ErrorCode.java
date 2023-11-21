c1abb55dc70727d0378ad153aa219ec94a47ec73
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-11-22 01:41:21 +0900

    feat: 중복시 에러 코드 및 메세지 추가

--- src/main/java/com/fithub/fithubbackend/global/exception/ErrorCode.java
+++ src/main/java/com/fithub/fithubbackend/global/exception/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
 
     NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 데이터입니다."),
     INVALID_DATE(HttpStatus.CONFLICT, "유효하지 않은 날짜입니다."),
-    UNCORRECTABLE_DATA(HttpStatus.CONFLICT, "현재 수정할 수 없는 데이터입니다.");
+    UNCORRECTABLE_DATA(HttpStatus.CONFLICT, "현재 수정할 수 없는 데이터입니다."),
+    DUPLICATE(HttpStatus.CONFLICT,"중복된 데이터 입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
