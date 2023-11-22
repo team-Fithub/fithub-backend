8266bfa50282be7a60b6430421eb521d5c354752
강지륜 <kjr2699@naver.com>
2023-11-22 23:40:19 +0900

    feat: jwt 토큰 관련 ErrorCode 추가

--- src/main/java/com/fithub/fithubbackend/global/exception/ErrorCode.java
+++ src/main/java/com/fithub/fithubbackend/global/exception/ErrorCode.java
@@ -10,7 +10,9 @@ public enum ErrorCode {
 
     NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 데이터입니다."),
     INVALID_DATE(HttpStatus.CONFLICT, "유효하지 않은 날짜입니다."),
-    UNCORRECTABLE_DATA(HttpStatus.CONFLICT, "현재 수정할 수 없는 데이터입니다.");
+    UNCORRECTABLE_DATA(HttpStatus.CONFLICT, "현재 수정할 수 없는 데이터입니다."),
+    FAIL_AUTHENTICATION(HttpStatus.UNAUTHORIZED, "검증되지 않는 토큰입니다."),
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
