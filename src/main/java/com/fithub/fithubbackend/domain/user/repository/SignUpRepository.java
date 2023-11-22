2a4faaa43d7564fdbb023bbc1302b6291be7d9ce
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-11-22 23:11:21 +0900

    fix: 레파지토리 접근 시 에러 수정
    boolean -> Domain

--- src/main/java/com/fithub/fithubbackend/domain/user/repository/SignUpRepository.java
+++ src/main/java/com/fithub/fithubbackend/domain/user/repository/SignUpRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface SignUpRepository extends JpaRepository<User,Long> {
-    boolean findByUserId(String userId);
-    boolean findByNickName(String nickname);
-    boolean findByEmail(String email);
+    User findByUserId(String userId);
+    User findByNickname(String nickname);
+    User findByEmail(String email);
 }
