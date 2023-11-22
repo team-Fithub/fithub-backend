76c802bbec3b3b33d518aa4da2e725cdf0dd8462
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-11-22 23:05:32 +0900

    feat: 이메일 정규식 추가 및 비밀번호 정규식 변경호 정규식 변경

--- src/main/java/com/fithub/fithubbackend/domain/user/dto/SignUpDto.java
+++ src/main/java/com/fithub/fithubbackend/domain/user/dto/SignUpDto.java
@@ -1,41 +1,41 @@
 package com.fithub.fithubbackend.domain.user.dto;
 
 import com.fithub.fithubbackend.domain.user.domain.Gender;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class SignUpDto {
-    private final String PASSWORD_REGEXP = "^[a-zA-Z0-9!@#$%^&*]{8,16}$";
+    private final String PASSWORD_REGEXP = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@!%*#?&])[A-Za-z\\d@!%*#?&]{8,}$";
     private final String PHONE_NUMBER_REGEXP = "^\\d{3}-\\d{3,4}-\\d{4}$";
+    private final String EMAIL_REGEXP = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
     private final String NAME_REGEXP = "^[a-zA-Z0-9가-힣]$";
 
-    @NotEmpty(message = "아이디 입력은 필수입니다.")
+    @NotBlank(message = "아이디 입력은 필수입니다.")
     private String userId;
 
-    @NotEmpty(message = "이름 입력은 필수입니다.")
+    @NotBlank(message = "이름 입력은 필수입니다.")
     @Pattern(regexp = NAME_REGEXP, message = "특수문자는 포함될 수 없습니다.")
     private String name;
 
-    @NotEmpty(message = "닉네임 입력은 필수입니다.")
+    @NotBlank(message = "닉네임 입력은 필수입니다.")
     private String nickname;
 
-    @NotEmpty(message = "이메일 입력은 필수입니다.")
-    @Email(message = "이메일 형식에 맞지 않습니다.")
+    @NotBlank(message = "이메일 입력은 필수입니다.")
+    @Pattern(regexp = EMAIL_REGEXP, message = "이메일 형식에 맞지 않습니다.")
     private String email;
 
-    @NotEmpty(message = "비밀번호 입력은 필수입니다.")
-    @Pattern(regexp = PASSWORD_REGEXP, message = "비밀번호 형식에 맞지 않습니다.")
+    @NotBlank(message = "비밀번호 입력은 필수입니다.")
+    @Pattern(regexp = PASSWORD_REGEXP, message = "비밀번호 형식에 맞지 않습니다.(8자 이상 특수문자 포함)")
     private String password;
 
-    @NotEmpty(message = "전화번호 입력은 필수입니다.")
+    @NotBlank(message = "전화번호 입력은 필수입니다.")
     @Pattern(regexp = PHONE_NUMBER_REGEXP, message = "전화번호 형식에 맞지 않습니다.")
     private String phone;
 
