ddfde4e4b3c64fd0ddcacc8ba38e89b8355d4bc4
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-11-25 14:51:33 +0900

    refactor: swagger 추가

--- src/main/java/com/fithub/fithubbackend/domain/user/api/EmailController.java
+++ src/main/java/com/fithub/fithubbackend/domain/user/api/EmailController.java
@@ -3,6 +3,12 @@ package com.fithub.fithubbackend.domain.user.api;
 import com.fithub.fithubbackend.domain.user.application.EmailServiceImpl;
 import com.fithub.fithubbackend.domain.user.dto.EmailDto;
 import com.fithub.fithubbackend.domain.user.dto.EmailNumberDto;
+import com.fithub.fithubbackend.global.exception.ErrorResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,12 +22,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class EmailController {
     private final EmailServiceImpl emailService;
-
+    @Operation(summary = "이메일 전송(인증번호)", responses = {
+            @ApiResponse(responseCode = "200", description = "전송완료"),
+    })
     @PostMapping("/email/send")
     public void sendEmail(@RequestBody EmailDto emailDto) throws MessagingException {
         emailService.sendEmail(emailDto);
     }
-
+    @Operation(summary = "생성된 인증번호와 입력한 인증번호 비교", responses = {
+            @ApiResponse(responseCode = "200", description = "생성됨"),
+            @ApiResponse(responseCode = "409", description = "생성된 인증번호와 일치하지 않음", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+    })
     @PostMapping("/email/check")
     public ResponseEntity<String> checkCertificationNumber(@RequestBody EmailNumberDto emailNumberDto){
         return emailService.checkCertificationNumber(emailNumberDto);
--- src/main/java/com/fithub/fithubbackend/domain/user/api/SignUpController.java
+++ src/main/java/com/fithub/fithubbackend/domain/user/api/SignUpController.java
@@ -3,6 +3,12 @@ package com.fithub.fithubbackend.domain.user.api;
 import com.fithub.fithubbackend.domain.user.application.SignUpServiceImpl;
 import com.fithub.fithubbackend.domain.user.dto.SignUpDto;
 import com.fithub.fithubbackend.domain.user.dto.SignUpResponseDto;
+import com.fithub.fithubbackend.global.exception.ErrorResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +24,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class SignUpController {
     private final SignUpServiceImpl signUpService;
 
+    @Operation(summary = "회원가입", responses = {
+            @ApiResponse(responseCode = "200", description = "회원 생성"),
+            @ApiResponse(responseCode = "409", description = "이메일 중복", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))),
+            @ApiResponse(responseCode = "409", description = "닉네임 중복", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))),
+            @ApiResponse(responseCode = "409", description = "아이디 중복", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))),
+            @ApiResponse(responseCode = "409", description = "형식 에러 (이메일 형식 , 비밀번호 형식(8자이상 특수문자 포함), 닉네임 형식(특수문자 제외 한글, 영어, 숫자), 전화번호 형식 (xxx-xxx(xxxx)-xxxx)", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+    })
     @PostMapping("/signup")
     public ResponseEntity<SignUpResponseDto> signUp(@RequestBody @Valid SignUpDto signUpDto, BindingResult bindingResult){
         return signUpService.signUp(signUpDto,bindingResult);
