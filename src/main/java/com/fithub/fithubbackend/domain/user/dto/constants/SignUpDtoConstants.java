d0f6462fab73345e1213326d0ff7db7d5b814b24
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-11-30 22:39:32 +0900

    refactor: 전화번호 정규식 수정('-' 제거)

--- src/main/java/com/fithub/fithubbackend/domain/user/dto/constants/SignUpDtoConstants.java
+++ src/main/java/com/fithub/fithubbackend/domain/user/dto/constants/SignUpDtoConstants.java
@@ -2,7 +2,7 @@ package com.fithub.fithubbackend.domain.user.dto.constants;
 
 public class SignUpDtoConstants {
     public static final String PASSWORD_REGEXP = "^(?=.*[@$!%*#?&])[A-Za-z@$!%*#?&\\d]{8,}$";
-    public static final String PHONE_NUMBER_REGEXP = "^\\d{3}-\\d{3,4}-\\d{4}$";
+    public static final String PHONE_NUMBER_REGEXP = "^\\d{3}\\d{3,4}\\d{4}$";
     public static final String EMAIL_REGEXP = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
     public static final String NAME_REGEXP = "^[a-zA-Z가-힣]*$";
     public static final String FORM_DATA_ERROR_REGEXP = "[Optional\\[\\]]";
