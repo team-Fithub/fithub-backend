0a9fbdf5127e79e3d47797d660fa2298396a8863
강지륜 <kjr2699@naver.com>
2023-12-04 22:48:07 +0900

    update: 소셜 로그인 시 redis 키 값을 OAuth2AuthenticationToken 대신 email으로 수정

--- src/main/java/com/fithub/fithubbackend/global/auth/OAuthSuccessHandler.java
+++ src/main/java/com/fithub/fithubbackend/global/auth/OAuthSuccessHandler.java
@@ -9,11 +9,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Component
@@ -30,12 +33,13 @@ public class OAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-//        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
 
         TokenInfoDto tokenInfoDto = jwtTokenProvider.createToken(authentication);
         cookieUtil.addRefreshTokenCookie(response, tokenInfoDto);
         cookieUtil.addAccessTokenCookie(response, tokenInfoDto.getAccessToken());
-        redisUtil.setData(authentication.getName(), tokenInfoDto.getRefreshToken(), tokenInfoDto.getRefreshTokenExpirationTime());
+
+        redisUtil.setData((String) oAuth2User.getAttributes().get("email"), tokenInfoDto.getRefreshToken(), tokenInfoDto.getRefreshTokenExpirationTime());
 
         String targetUrl = UriComponentsBuilder.fromUriString(url)
                 .queryParam("accessToken", tokenInfoDto.getAccessToken())
