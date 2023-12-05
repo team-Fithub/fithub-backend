03ae9da1a34b58a95de95a9ef60a0299e89fcc95
mohyerolo <garam2101@naver.com>
2023-12-05 18:13:37 +0900

    feat: 트레이닝 찜 여부 조회, 찜 설정/해제 기능 추가

--- src/main/java/com/fithub/fithubbackend/domain/Training/application/UserTrainingServiceImpl.java
+++ src/main/java/com/fithub/fithubbackend/domain/Training/application/UserTrainingServiceImpl.java
@@ -1,9 +1,13 @@
 package com.fithub.fithubbackend.domain.Training.application;
 
 import com.fithub.fithubbackend.domain.Training.domain.Training;
+import com.fithub.fithubbackend.domain.Training.domain.TrainingLikes;
 import com.fithub.fithubbackend.domain.Training.dto.TrainingInfoDto;
 import com.fithub.fithubbackend.domain.Training.dto.TrainingOutlineDto;
+import com.fithub.fithubbackend.domain.Training.repository.TrainingLikesRepository;
 import com.fithub.fithubbackend.domain.Training.repository.TrainingRepository;
+import com.fithub.fithubbackend.domain.user.domain.User;
+import com.fithub.fithubbackend.domain.user.repository.UserRepository;
 import com.fithub.fithubbackend.global.exception.CustomException;
 import com.fithub.fithubbackend.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +21,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserTrainingServiceImpl implements UserTrainingService {
 
     private final TrainingRepository trainingRepository;
+    private final TrainingLikesRepository trainingLikesRepository;
+    private final UserRepository userRepository;
 
     @Override
     @Transactional(readOnly = true)
@@ -32,4 +38,34 @@ public class UserTrainingServiceImpl implements UserTrainingService {
         return TrainingInfoDto.toDto(training);
     }
 
+    @Override
+    @Transactional(readOnly = true)
+    public boolean isLikesTraining(Long trainingId, String email) {
+        Training training = trainingRepository.findById(trainingId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND, "존재하지 않는 트레이닝입니다."));
+        checkClosed(training.isClosed());
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND, "존재하지 않는 회원입니다."));
+        return trainingLikesRepository.existsByTrainingIdAndUserId(trainingId, user.getId());
+    }
+
+    @Override
+    @Transactional
+    public void likesTraining(Long trainingId, boolean likes, String email) {
+        Training training = trainingRepository.findById(trainingId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND, "존재하지 않는 트레이닝입니다."));
+        checkClosed(training.isClosed());
+
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND, "존재하지 않는 회원입니다."));
+        if (likes) {
+            TrainingLikes trainingLikes = TrainingLikes.builder().training(training).user(user).build();
+            trainingLikesRepository.save(trainingLikes);
+        } else {
+            TrainingLikes trainingLikes = trainingLikesRepository.findByTrainingIdAndUserId(trainingId, user.getId()).orElseThrow(() -> new CustomException(ErrorCode.UNCORRECTABLE_DATA, "트레이닝을 찜하지 않았습니다."));
+            trainingLikesRepository.delete(trainingLikes);
+        }
+    }
+
+    private void checkClosed(boolean closed) {
+        if (closed) {
+            throw new CustomException(ErrorCode.UNCORRECTABLE_DATA, "마감된 트레이닝은 찜할 수 없습니다.");
+        }
+    }
 }
