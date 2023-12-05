300a869926275239b7b7c4eb69452a52b43a0930
강지륜 <kjr2699@naver.com>
2023-12-06 01:02:26 +0900

    feat: gender 타입 변경 메소드 추가

--- src/main/java/com/fithub/fithubbackend/domain/user/enums/Gender.java
+++ src/main/java/com/fithub/fithubbackend/domain/user/enums/Gender.java
@@ -1,5 +1,16 @@
 package com.fithub.fithubbackend.domain.user.enums;
 
 public enum Gender {
-    F, M, UNDEFINED
+    F, M, UNDEFINED;
+
+    public static Gender toGender(String gender) {
+        if (gender != null) {
+            switch (gender) {
+                case "F": return F;
+                case "M": return M;
+                default: return UNDEFINED;
+            }
+        }
+        return UNDEFINED;
+    }
 }
