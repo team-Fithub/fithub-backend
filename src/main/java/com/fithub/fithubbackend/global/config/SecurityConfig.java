ed6c4af3d0e7d0c25f28569f0a68f1c945696ab6
mohyerolo <garam2101@naver.com>
2023-12-11 19:10:44 +0900

    fix: user role을 USER -> ROLE_USER로 수정

--- src/main/java/com/fithub/fithubbackend/global/config/SecurityConfig.java
+++ src/main/java/com/fithub/fithubbackend/global/config/SecurityConfig.java
@@ -67,6 +67,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 HttpMethod.GET, PERMIT_ALL_GET_PATTERNS).permitAll()
                         .requestMatchers("/**").hasRole("USER")
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(e -> e.authenticationEntryPoint(new CustomAuthenticationEntryPoint()))
