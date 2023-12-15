b990780d25fea0bfc68b16e4f7be412f570e873d
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-12-15 17:39:03 +0900

    update: Swagger 추가

--- src/main/java/com/fithub/fithubbackend/domain/user/api/MapController.java
+++ src/main/java/com/fithub/fithubbackend/domain/user/api/MapController.java
@@ -3,6 +3,8 @@ package com.fithub.fithubbackend.domain.user.api;
 import com.fithub.fithubbackend.domain.user.application.MapService;
 import com.fithub.fithubbackend.domain.user.dto.constants.MapDocumentDto;
 import com.fithub.fithubbackend.domain.user.dto.constants.MapDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,11 @@ import java.util.List;
 @RequestMapping("/auth")
 public class MapController {
     private final MapService mapService;
+
+    @Operation(summary = "헬스장 조회", responses = {
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "409", description = "JSONParsing 에러")
+    })
     @GetMapping("/map")
     public ResponseEntity<MapDto> getFitness(@RequestParam(value = "page",defaultValue = "1") int page, @RequestParam double x, @RequestParam double y) {
         return ResponseEntity.ok(mapService.getLocationByFitness(page,x,y));
