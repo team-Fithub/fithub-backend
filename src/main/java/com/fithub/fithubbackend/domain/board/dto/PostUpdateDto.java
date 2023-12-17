4c7de984c6fe8fc8eff2b2fba63c7a51e1367ef2
강지륜 <kjr2699@naver.com>
2023-12-17 18:25:01 +0900

    update: 게시글 생성 및 수정 dto 수정

--- src/main/java/com/fithub/fithubbackend/domain/board/dto/PostUpdateDto.java
+++ src/main/java/com/fithub/fithubbackend/domain/board/dto/PostUpdateDto.java
@@ -1,26 +1,31 @@
 package com.fithub.fithubbackend.domain.board.dto;
 
-import com.fithub.fithubbackend.domain.user.domain.User;
+import com.fithub.fithubbackend.domain.board.post.domain.PostDocument;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import java.util.*;
 
 @Data
-@Schema(description = "게시글 생성 dto")
-public class PostCreateDto {
+@Schema(description = "게시글 수정 dto")
+public class PostUpdateDto {
+    @NotNull
+    @Schema(description = "게시글 id")
+    private Long id;
 
     @NotNull
     @Schema(description = "게시글 내용")
     private String content;
 
-    @Schema(description = "게시글 해시태그")
+    @Schema(description = "게시글 작성자")
+    private String userId;
+
+    @Schema(description = "게시글 헤시태그")
     private String hashTags;
 
     @Schema(description = "게시글 이미지")
     private List<MultipartFile> images;
-
 
 }
