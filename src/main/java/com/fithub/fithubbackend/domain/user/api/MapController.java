35b4c81e846631a15454bfe69addcb0b445f280c
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2023-12-18 21:11:11 +0900

    rename: 파일위치 변경

--- src/main/java/com/fithub/fithubbackend/domain/user/api/MapController.java
+++ src/main/java/com/fithub/fithubbackend/domain/user/api/MapController.java
@@ -1,8 +1,7 @@
 package com.fithub.fithubbackend.domain.user.api;
 
 import com.fithub.fithubbackend.domain.user.application.MapService;
-import com.fithub.fithubbackend.domain.user.dto.constants.MapDocumentDto;
-import com.fithub.fithubbackend.domain.user.dto.constants.MapDto;
+import com.fithub.fithubbackend.domain.user.dto.MapDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
--- src/main/java/com/fithub/fithubbackend/domain/user/application/MapService.java
+++ src/main/java/com/fithub/fithubbackend/domain/user/application/MapService.java
@@ -1,8 +1,6 @@
 package com.fithub.fithubbackend.domain.user.application;
 
-import com.fithub.fithubbackend.domain.user.dto.constants.MapDto;
-
-import java.util.List;
+import com.fithub.fithubbackend.domain.user.dto.MapDto;
 
 public interface MapService {
     MapDto getLocationByFitness(int page,double x, double y);
