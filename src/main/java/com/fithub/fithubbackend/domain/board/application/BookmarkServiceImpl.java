131e40b81e297259b1ee25e667e7b319b738a310
강지륜 <kjr2699@naver.com>
2023-12-27 18:02:52 +0900

    feat: 북마크 기능 추가

---
+++ src/main/java/com/fithub/fithubbackend/domain/board/application/BookmarkServiceImpl.java
@@ -0,0 +1,48 @@
+package com.fithub.fithubbackend.domain.board.application;
+
+import com.fithub.fithubbackend.domain.board.post.domain.Bookmark;
+import com.fithub.fithubbackend.domain.board.post.domain.Post;
+import com.fithub.fithubbackend.domain.board.repository.BookmarkRepository;
+import com.fithub.fithubbackend.domain.user.domain.User;
+import com.fithub.fithubbackend.global.exception.CustomException;
+import com.fithub.fithubbackend.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Description;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class BookmarkServiceImpl implements BookmarkService {
+
+    private final BookmarkRepository bookmarkRepository;
+
+    @Override
+    @Transactional
+    public void addBookmark(User user, Post post) {
+
+        Optional<Bookmark> bookmark = bookmarkRepository.findByUserAndPost(user, post);
+
+        if (bookmark.isPresent())
+            throw new CustomException(ErrorCode.DUPLICATE, "이미 북마크한 게시글입니다.");
+
+        bookmarkRepository.save(new Bookmark(user, post));
+    }
+
+    @Override
+    @Transactional
+    public void deleteBookmark(User user, Post post) {
+        bookmarkRepository.deleteByUserAndPost(user, post);
+    }
+
+
+    @Override
+    @Description("회원의 북마크 기록 조회 (내림차순)")
+    @Transactional
+    public List<Bookmark> getBookmarksByUser(User user) {
+        return bookmarkRepository.findByUserOrderByCreatedDateDesc(user);
+    }
+}
