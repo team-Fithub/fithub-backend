d4e20d417e8142d60cb6fb1078fedb8e3fc62687
mohyerolo <garam2101@naver.com>
2024-01-02 13:59:44 +0900

    feat: valid 에러 핸들러 추가

--- src/main/java/com/fithub/fithubbackend/global/exception/CustomExceptionHandler.java
+++ src/main/java/com/fithub/fithubbackend/global/exception/CustomExceptionHandler.java
@@ -2,6 +2,8 @@ package com.fithub.fithubbackend.global.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,5 +15,13 @@ public class CustomExceptionHandler {
     public ResponseEntity<ErrorResponseDto> handleCustomException(CustomException exception) {
         log.info(exception.toString());
         return ErrorResponseDto.toResponseEntity(exception.getErrorCode(), exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> handleValidException(MethodArgumentNotValidException exception) {
+        log.info("[CustomExceptionHandler] - @Valid 에러");
+        BindingResult bindingResult = exception.getBindingResult();
+        String message = bindingResult.getFieldError().getDefaultMessage();
+        return ErrorResponseDto.toResponseEntity(ErrorCode.INVALID_FORM_DATA, exception.getMessage());
     }
 }
