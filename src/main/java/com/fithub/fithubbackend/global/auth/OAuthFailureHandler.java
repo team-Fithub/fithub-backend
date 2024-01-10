9be71fed29f615e0856a8b6bd8e5f1f3c60da124
mohyerolo <garam2101@naver.com>
2024-01-10 22:27:45 +0900

    feat: oauth2 로그인 실패 핸들러, 권한 에러 핸들러 추가

--- src/main/java/com/fithub/fithubbackend/global/auth/OAuthFailureHandler.java
+++ src/main/java/com/fithub/fithubbackend/global/auth/OAuthFailureHandler.java
@@ -8,23 +8,22 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-@Component
 @Slf4j
-public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+@Component
+public class OAuthFailureHandler implements AuthenticationFailureHandler  {
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response,
-                         AuthenticationException authException) throws IOException, ServletException {
-        log.info("[AuthenticationEntryPoint] - 인증 실패: {}, endPoint: {}", authException.getMessage(), request.getServletPath());
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        log.error("소셜 로그인 실패: {} , endpoint: {}", exception.getMessage(), request.getServletPath());
         ObjectMapper objectMapper = new ObjectMapper();
-        ErrorResponseDto errorResponseDto = ErrorResponseDto.toResponseEntity(ErrorCode.AUTHENTICATION_ERROR, "인증 실패").getBody();
+        ErrorResponseDto errorResponseDto = ErrorResponseDto.toResponseEntity(ErrorCode.BAD_REQUEST, exception.getMessage()).getBody();
 
-        response.setStatus(401);
+        response.setStatus(400);
         response.setContentType("application/json");
         response.setCharacterEncoding("utf-8");
         response.getWriter().write(objectMapper.writeValueAsString(errorResponseDto));
