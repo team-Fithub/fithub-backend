19519a7676c9d30bb68a061656048986e767cd60
mohyerolo <garam2101@naver.com>
2024-01-16 12:39:33 +0900

    update: 소셜 로그인 실패 시 실패 url 반환(status,message포함)

--- src/main/java/com/fithub/fithubbackend/global/auth/OAuthFailureHandler.java
+++ src/main/java/com/fithub/fithubbackend/global/auth/OAuthFailureHandler.java
@@ -1,31 +1,30 @@
 package com.fithub.fithubbackend.global.auth;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fithub.fithubbackend.global.exception.ErrorCode;
-import com.fithub.fithubbackend.global.exception.ErrorResponseDto;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 
 @Slf4j
 @Component
-public class OAuthFailureHandler implements AuthenticationFailureHandler  {
+public class OAuthFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         log.error("소셜 로그인 실패: {} , endpoint: {}", exception.getMessage(), request.getServletPath());
-        ObjectMapper objectMapper = new ObjectMapper();
-        ErrorResponseDto errorResponseDto = ErrorResponseDto.toResponseEntity(ErrorCode.BAD_REQUEST, exception.getMessage()).getBody();
 
-        response.setStatus(400);
-        response.setContentType("application/json");
-        response.setCharacterEncoding("utf-8");
-        response.getWriter().write(objectMapper.writeValueAsString(errorResponseDto));
+        String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:3000/oauth/fail")
+                .queryParam("status", HttpStatus.BAD_REQUEST)
+                .queryParam("message", exception.getMessage())
+                .build()
+                .encode().toString();
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
 }
