0d4ec2c717976299e4ce6102809dea2d8b3e4e66
강지륜 <kjr2699@naver.com>
2024-02-01 02:14:47 +0900

    update: 답글 dto에 부모 댓글 작성자 닉네임 field 추가

--- src/main/java/com/fithub/fithubbackend/domain/board/dto/CommentInfoDto.java
+++ src/main/java/com/fithub/fithubbackend/domain/board/dto/CommentInfoDto.java
@@ -2,48 +2,63 @@ package com.fithub.fithubbackend.domain.board.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fithub.fithubbackend.domain.board.comment.domain.Comment;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.*;
 
 @Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(description = "답글 정보 dto")
 public class CommentInfoDto {
 
     private Long commentId;
 
     private String writerNickName;
 
+    private String writerProfileUrl;
+
     private String content;
 
-    private Long parentCommentId;
+    private String mentionedUserNickname;
 
-    private String profileUrl;
-
-    private String profileInputName;
 
     private boolean deleted;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdDate;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime modifiedDate;
 
-    private List<CommentInfoDto> childComment = new ArrayList<>();
+    @Builder
+    public CommentInfoDto(Long commentId, String writerNickName, String content, String mentionedUserNickname,
+                          String writerProfileUrl, boolean deleted, LocalDateTime createdDate,
+                          LocalDateTime modifiedDate) {
+        this.commentId = commentId;
+        this.writerNickName = writerNickName;
+        this.content = content;
+        this.writerProfileUrl = writerProfileUrl;
+        this.deleted = deleted;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
+        this.mentionedUserNickname = mentionedUserNickname;
+    }
 
+    public static CommentInfoDto toDto(Comment comment) {
 
-    public static CommentInfoDto fromEntity(Comment comment) {
-        CommentInfoDto commentInfoDto = new CommentInfoDto();
-
-        commentInfoDto.setCommentId(comment.getId());
-        commentInfoDto.setDeleted(comment.getDeleted() == null ? false : comment.getDeleted());
-        commentInfoDto.setWriterNickName(comment.getUser().getNickname());
-        commentInfoDto.setProfileUrl(comment.getUser().getProfileImg().getUrl());
-        commentInfoDto.setProfileInputName(comment.getUser().getProfileImg().getInputName());
-        commentInfoDto.setContent(comment.getContent());
-        commentInfoDto.setParentCommentId(comment.getParent() == null ? null : comment.getParent().getId());
-        commentInfoDto.setCreatedDate(comment.getCreatedDate());
-
-        return commentInfoDto;
+        return CommentInfoDto.builder()
+                .commentId(comment.getId())
+                .writerNickName(comment.getUser().getNickname())
+                .content(comment.getContent())
+                .writerProfileUrl(comment.getUser().getProfileImg().getUrl())
+                .createdDate(comment.getCreatedDate())
+                .modifiedDate(comment.getModifiedDate())
+                .mentionedUserNickname(comment.getParent().getUser().getNickname())
+                .deleted(comment.getDeleted() == null ? false : comment.getDeleted()).build();
     }
 
 
