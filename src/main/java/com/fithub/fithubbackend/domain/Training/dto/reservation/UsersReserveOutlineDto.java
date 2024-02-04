6e56dbcff5d72bf25acd7bfbb7debbcf250dce02
mohyerolo <garam2101@naver.com>
2024-02-04 16:32:31 +0900

    feat: 예약 조회(page) customRepository에 추가

--- src/main/java/com/fithub/fithubbackend/domain/Training/dto/reservation/UsersReserveOutlineDto.java
+++ src/main/java/com/fithub/fithubbackend/domain/Training/dto/reservation/UsersReserveOutlineDto.java
@@ -1,18 +1,22 @@
 package com.fithub.fithubbackend.domain.Training.dto.reservation;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fithub.fithubbackend.domain.Training.domain.ReserveInfo;
 import com.fithub.fithubbackend.domain.Training.enums.ReserveStatus;
+import com.querydsl.core.annotations.QueryProjection;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
+@RequiredArgsConstructor
+@AllArgsConstructor
 @Builder
 @Schema(description = "회원의 트레이닝 예약, 진행,종료 정보 확인")
-public class UsersReserveInfoDto {
+public class UsersReserveOutlineDto {
 
     @Schema(description = "트레이닝 예약 id")
     private Long reservationId;
@@ -27,29 +31,30 @@ public class UsersReserveInfoDto {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime reserveDateTime;
 
+    private String location;
+
     @Schema(description = "트레이닝 진행 상황(진행 전, 진행중, 진행완료)")
     private ReserveStatus status;
-
-    @Schema(description = "트레이닝 결제 금액")
-    private int price;
-
-    @Schema(description = "트레이닝 구매 번호")
-    private String merchantUid;
 
     @Schema(description = "트레이닝 결제(예약)한 날짜, 시간")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime paymentDateTime;
 
-    public static UsersReserveInfoDto toDto(ReserveInfo reserveInfo) {
-        return UsersReserveInfoDto.builder()
-                .reservationId(reserveInfo.getId())
-                .trainingId(reserveInfo.getTraining().getId())
-                .title(reserveInfo.getTraining().getTitle())
-                .reserveDateTime(reserveInfo.getReserveDateTime())
-                .status(reserveInfo.getStatus())
-                .price(reserveInfo.getPrice())
-                .merchantUid(reserveInfo.getMerchantUid())
-                .paymentDateTime(reserveInfo.getCreatedDate())
-                .build();
+    @Schema(description = "트레이닝 결제 변경 날짜, 시간 (취소, 노쇼 처리 시 이걸 참고)")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime modifiedDateTime;
+
+    @QueryProjection
+    public UsersReserveOutlineDto(Long reservationId, Long trainingId, String title, String location,
+                                  LocalDateTime reserveDateTime, ReserveStatus status,
+                                  LocalDateTime paymentDateTime, LocalDateTime modifiedDateTime) {
+        this.reservationId = reservationId;
+        this.trainingId = trainingId;
+        this.title = title;
+        this.location = location;
+        this.reserveDateTime = reserveDateTime;
+        this.status = status;
+        this.paymentDateTime = paymentDateTime;
+        this.modifiedDateTime = modifiedDateTime;
     }
 }
