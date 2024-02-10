b01c96cf3b371055f10b85315076cd3083be17e2
mohyerolo <garam2101@naver.com>
2024-02-10 21:40:02 +0900

    update: 트레이닝 찜 설정,해제 api response 추가

--- src/main/java/com/fithub/fithubbackend/domain/Training/application/UserTrainingLikeServiceImpl.java
+++ src/main/java/com/fithub/fithubbackend/domain/Training/application/UserTrainingLikeServiceImpl.java
@@ -37,6 +37,10 @@ public class UserTrainingLikeServiceImpl implements UserTrainingLikeService {
         Training training = trainingRepository.findById(trainingId).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND, "존재하지 않는 트레이닝입니다."));
         checkClosed(training.isClosed());
 
+        if (trainingLikesRepository.existsByTrainingIdAndUserId(trainingId, user.getId())) {
+            throw new CustomException(ErrorCode.DUPLICATE, "이미 찜 설정이 되어있습니다.");
+        }
+
         if (user.getId().equals(training.getTrainer().getUser().getId())) {
             throw new CustomException(ErrorCode.UNKNOWN_ERROR, "트레이너는 자신의 트레이닝을 찜할 수 없습니다.");
         }
@@ -57,7 +61,8 @@ public class UserTrainingLikeServiceImpl implements UserTrainingLikeService {
         if (!trainingRepository.existsById(trainingId)) {
             throw new CustomException(ErrorCode.NOT_FOUND, "존재하지 않는 트레이닝입니다.");
         }
-        TrainingLikes trainingLikes = trainingLikesRepository.findByTrainingIdAndUserId(trainingId, user.getId()).orElseThrow(() -> new CustomException(ErrorCode.UNCORRECTABLE_DATA, "트레이닝을 찜하지 않았습니다."));
+        TrainingLikes trainingLikes = trainingLikesRepository.findByTrainingIdAndUserId(trainingId, user.getId())
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND, "해당 트레이닝을 찜하지 않았습니다."));
         trainingLikesRepository.delete(trainingLikes);
     }
 
