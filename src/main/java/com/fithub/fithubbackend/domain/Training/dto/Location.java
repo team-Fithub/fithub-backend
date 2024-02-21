f1cb782945e50044262772f68d1d385fe4858265
mohyerolo <garam2101@naver.com>
2024-02-21 21:42:27 +0900

    update: swagger 설명 추가

--- src/main/java/com/fithub/fithubbackend/domain/Training/dto/Location.java
+++ src/main/java/com/fithub/fithubbackend/domain/Training/dto/Location.java
@@ -1,5 +1,6 @@
 package com.fithub.fithubbackend.domain.Training.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,6 +10,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Location {
+    @Schema(description = "위도", example = "37.573319")
     private Double latitude;
+
+    @Schema(description = "경도", example = "126.915444")
     private Double longitude;
 }
--- src/main/java/com/fithub/fithubbackend/domain/Training/dto/TrainingOutlineDto.java
+++ src/main/java/com/fithub/fithubbackend/domain/Training/dto/TrainingOutlineDto.java
@@ -2,6 +2,7 @@ package com.fithub.fithubbackend.domain.Training.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fithub.fithubbackend.domain.Training.domain.Training;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -16,6 +17,8 @@ public class TrainingOutlineDto {
     private String title;
     private int price;
     private String address;
+
+    @Schema(description = "보내준 위치와의 거리. 위치로 트레이닝 검색할 때만 값이 들어가있음")
     private Double dist;
 
     @JsonFormat(pattern = "yyyy-MM-dd")
