8775d15bb216c2ff506907e7729d6f7a1ae81caa
mohyerolo <garam2101@naver.com>
2024-02-21 17:49:08 +0900

    feat: 좌표 계산 시 필요한 파일들 추가

---
+++ src/main/java/com/fithub/fithubbackend/domain/Training/dto/Location.java
@@ -0,0 +1,11 @@
+package com.fithub.fithubbackend.domain.Training.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Location {
+    private Double latitude;
+    private Double longitude;
+}
