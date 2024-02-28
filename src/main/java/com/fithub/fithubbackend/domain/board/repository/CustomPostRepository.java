46598551b87251d8ba495df6214ff80c67dc8daa
강지륜 <kjr2699@naver.com>
2024-02-29 01:31:25 +0900

    update: 게시글 검색 기능 수정 (page 정보를 dto로 받는 대신 pageable 파라미터로 받도록 수정)

--- src/main/java/com/fithub/fithubbackend/domain/board/repository/CustomPostRepository.java
+++ src/main/java/com/fithub/fithubbackend/domain/board/repository/CustomPostRepository.java
@@ -3,9 +3,10 @@ package com.fithub.fithubbackend.domain.board.repository;
 import com.fithub.fithubbackend.domain.board.dto.PostSearchFilterDto;
 import com.fithub.fithubbackend.domain.board.post.domain.Post;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CustomPostRepository {
 
-    Page<Post> searchPostsByKeyword(PostSearchFilterDto postSearchFilterDTO);
+    Page<Post> searchPostsByKeyword(PostSearchFilterDto postSearchFilterDTO, Pageable pageable);
 
 }
--- src/main/java/com/fithub/fithubbackend/domain/board/repository/CustomPostRepositoryImpl.java
+++ src/main/java/com/fithub/fithubbackend/domain/board/repository/CustomPostRepositoryImpl.java
@@ -1,6 +1,5 @@
 package com.fithub.fithubbackend.domain.board.repository;
 
-import com.fithub.fithubbackend.domain.board.dto.PageableDto;
 import com.fithub.fithubbackend.domain.board.dto.PostSearchFilterDto;
 import com.fithub.fithubbackend.domain.board.post.domain.Post;
 import com.querydsl.core.QueryResults;
@@ -15,6 +14,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import static com.fithub.fithubbackend.domain.board.post.domain.QPost.post;
+import static com.fithub.fithubbackend.domain.board.comment.domain.QComment.comment;
 import static com.fithub.fithubbackend.domain.user.domain.QUser.user;
 
 import java.util.*;
@@ -32,16 +32,16 @@ public class CustomPostRepositoryImpl implements CustomPostRepository {
 
     @Override
     @Transactional(readOnly = true)
-    public Page<Post> searchPostsByKeyword(PostSearchFilterDto filter) {
+    public Page<Post> searchPostsByKeyword(PostSearchFilterDto filter, Pageable pageable) {
          QueryResults<Post> posts = jpaQueryFactory.selectFrom(post)
                 .where(scopeEq(filter))
                  .join(post.user, user).fetchJoin()
-                .orderBy(getOrderSpecifier(filter.getPageable()).toArray(OrderSpecifier[]::new))
-                .offset(filter.getPageable().getPage() * filter.getPageable().getSize())
-                .limit(filter.getPageable().getSize())
+                .orderBy(getOrderSpecifier(pageable).toArray(OrderSpecifier[]::new))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
                 .fetchResults();
 
-        return new PageImpl<>(posts.getResults(), PageRequest.of(filter.getPageable().getPage(), filter.getPageable().getSize()), posts.getTotal());
+        return new PageImpl<>(posts.getResults(), pageable, posts.getTotal());
     }
 
     private BooleanExpression scopeEq(PostSearchFilterDto filter) {
@@ -54,28 +54,26 @@ public class CustomPostRepositoryImpl implements CustomPostRepository {
     }
 
 
-    private List<OrderSpecifier> getOrderSpecifier(PageableDto pageable) {
+    private List<OrderSpecifier> getOrderSpecifier(Pageable pageable) {
 
         List<OrderSpecifier> orderSpecifiers = new ArrayList<>();
 
-
-        if (pageable == null || pageable.getSort() == null || pageable.getSort().isEmpty()) {
-            orderSpecifiers.add(post.id.desc());
-            return orderSpecifiers;
-        }
-
-        for (String sortProperty : pageable.getSort()) {
-            Sort.Order order = Sort.Order.by(sortProperty);
+        for (Sort.Order order : pageable.getSort()) {
+            Order direction = order.getDirection().isAscending() ? Order.ASC : Order.DESC;
 
             switch (order.getProperty()) {
+                case "id":
+                    orderSpecifiers.add(new OrderSpecifier<>(direction, post.id));
+                    break;
                 case "likes":
-                    orderSpecifiers.add(new OrderSpecifier<>(Order.DESC, post.likes.size()));
+                    orderSpecifiers.add(new OrderSpecifier<>(direction, post.likes.size()));
                     break;
                 case "comments":
-                    orderSpecifiers.add(new OrderSpecifier<>(Order.DESC,
-                            JPAExpressions.select(post.comments.size())
-                                    .from(post)
-                                    .where(post.comments.any().deleted.eq(false))));
+                    orderSpecifiers.add(
+                            new OrderSpecifier<>(direction,
+                                    JPAExpressions.select(comment.count())
+                                            .from(comment)
+                                            .where(comment.post.eq(post).and(comment.deleted.isNull()))));
                     break;
             }
         }
