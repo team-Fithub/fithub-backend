a7c0e4c14e1f98d4e558084858b3f526438449c5
강지륜 <kjr2699@naver.com>
2024-03-10 15:54:26 +0900

    update: 트레이닝 카테고리 수정 dto 설명 추가

--- src/main/java/com/fithub/fithubbackend/domain/Training/dto/trainersTraining/TrainingCategoryUpdateDto.java
+++ src/main/java/com/fithub/fithubbackend/domain/Training/dto/trainersTraining/TrainingCategoryUpdateDto.java
@@ -15,12 +15,12 @@ public class TrainingCategoryUpdateDto {
     @Schema(description = "삭제된 카테고리가 있다면 true 로 주면 됨.")
     private boolean categoryDeleted;
 
-    @Schema(description = "categoryDeleted = true 일 때만 주면 됨. 수정되지 않는 카테고리 리스트. ex) PILATES, HEALTH, PT, CROSSFIT, YOGA")
+    @Schema(description = "categoryDeleted = true 일 때만 주면 됨. (false 일 경우 빈 리스트로 전달) 수정되지 않는 카테고리 리스트. ex) PILATES, HEALTH, PT, CROSSFIT, YOGA")
     private List<Category> unModifiedCategoryList;
 
     @Schema(description = "새로 추가된 카테고리가 있다면 true 로 주면 됨.")
     private boolean categoryAdded;
 
-    @Schema(description = "categoryAdded = true 일 때만 주면 됨. 새로 추가한 카테고리. ex) PILATES, HEALTH, PT, CROSSFIT, YOGA")
+    @Schema(description = "categoryAdded = true 일 때만 주면 됨. (false 일 경우 빈 리스트로 전달) 새로 추가한 카테고리. ex) PILATES, HEALTH, PT, CROSSFIT, YOGA")
     private List<Category> newCategoryList;
 }
