238b0a83580dafa5400b9ac1d9d8c6ac5e7134fc
강지륜 <kjr2699@naver.com>
2024-03-15 03:19:24 +0900

    update: 소셜 회원가입 추가 정보 dto에 관심사 추가

--- src/main/java/com/fithub/fithubbackend/domain/user/dto/OAuthSignUpDto.java
+++ src/main/java/com/fithub/fithubbackend/domain/user/dto/OAuthSignUpDto.java
@@ -1,11 +1,15 @@
 package com.fithub.fithubbackend.domain.user.dto;
 
 import com.fithub.fithubbackend.domain.user.enums.Gender;
+import com.fithub.fithubbackend.global.common.Category;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 import static com.fithub.fithubbackend.domain.user.dto.constants.SignUpDtoConstants.*;
 
@@ -35,4 +39,9 @@ public class OAuthSignUpDto {
     @NotNull
     @Schema(description = "소셜 회원가입, 로그인시에 저장된 제공자 + id", example = "kakao_121211")
     private String providerId;
+
+    @NotEmpty(message = "관심사는 최소 1개 이상 선택해야 합니다. ex) PILATES, HEALTH, PT, CROSSFIT, YOGA")
+    @Schema(description = "관심사(최소 1개 이상). ex) PILATES, HEALTH, PT, CROSSFIT, YOGA")
+    private List<Category> interests;
+
 }
