66fbd76d5b419325fcac85012f681deb53c2af1c
강지륜 <kjr2699@naver.com>
2024-03-30 17:52:36 +0900

    update: 트레이너 추천 api 의 size 파리미터 기본 값(10) 설정

--- src/main/java/com/fithub/fithubbackend/domain/trainer/api/UserTrainerController.java
+++ src/main/java/com/fithub/fithubbackend/domain/trainer/api/UserTrainerController.java
@@ -24,12 +24,12 @@ public class UserTrainerController {
     @Operation(summary = "트레이너 추천", description = "회원이 설정한 관심사와 위치(반경 1.5km 이내), 트레이너 평점(3.5 이상)을 토대로 트레이너 추천(정렬 기준은 평점이 제일 높은 순으로). \n 관심사가 2개 이상인 경우 랜덤으로 한 개의 관심사 추출하여 선정",
             parameters = {
                     @Parameter(name = "location", description = "위도, 경도"),
-                    @Parameter(name = "size", description = "출력할 데이터의 수")
+                    @Parameter(name = "size", description = "출력할 데이터의 수. 안 보낼 경우 기본 10.")
             }, responses = {
             @ApiResponse(responseCode = "200", description = "추천 완료"),
     })
     @GetMapping("/trainer/recommendation")
-    public ResponseEntity<TrainerRecommendationOutlineDto> recommendTrainers(@AuthUser User user, @ModelAttribute Location location, @RequestParam("size") int size) {
+    public ResponseEntity<TrainerRecommendationOutlineDto> recommendTrainers(@AuthUser User user, @ModelAttribute Location location, @RequestParam(value = "size", defaultValue = "10") int size) {
         return ResponseEntity.ok().body(userTrainerService.recommendTrainers(user, location, size));
     }
 }
