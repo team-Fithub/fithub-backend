d455a4fc63ec64039164500ff6ab7121acd13ac1
Songhee Lee <clasonghee@gmail.com>
2024-04-16 22:43:16 +0900

    update: 채팅 메세지 조회 오름차순으로 변경, 메세지에 isMe 데이터 추가

--- src/main/java/com/fithub/fithubbackend/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/fithub/fithubbackend/domain/chat/repository/ChatMessageRepository.java
@@ -3,8 +3,6 @@ package com.fithub.fithubbackend.domain.chat.repository;
 import com.fithub.fithubbackend.domain.chat.domain.ChatMessage;
 import com.fithub.fithubbackend.domain.chat.domain.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
