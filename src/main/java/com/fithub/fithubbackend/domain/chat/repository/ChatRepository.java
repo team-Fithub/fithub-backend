13d191d1dd761f7abb37d9555bb0f8bd8583b6d6
mohyerolo <garam2101@naver.com>
2024-04-23 16:37:54 +0900

    fix fix: chatRepository 쿼리 에러 수정

--- src/main/java/com/fithub/fithubbackend/domain/chat/repository/ChatRepository.java
+++ src/main/java/com/fithub/fithubbackend/domain/chat/repository/ChatRepository.java
@@ -1,19 +1,22 @@
 package com.fithub.fithubbackend.domain.chat.repository;
 
 import com.fithub.fithubbackend.domain.chat.domain.Chat;
+import com.fithub.fithubbackend.domain.chat.domain.ChatRoom;
 import com.fithub.fithubbackend.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
-    @Query("SELECT e FROM Chat e WHERE e.chatPK.user.id = :userId")
-    List<Chat> findChatsById(Long userId);
+    List<Chat> findByChatPK_UserId(Long userId);
 
     @Query("SELECT e.chatPK.user FROM Chat e WHERE e.chatPK.chatRoom.roomId = :roomId")
-    List<User> findUsersByRoomId(Long roomId);
+    List<User> findUsersByRoomId(@Param("roomId") Long roomId);
 
     Chat findChatByChatPK_UserId(Long userId);
+
+    List<Chat> findByChatPK_ChatRoom(ChatRoom chatRoom);
 
 }
